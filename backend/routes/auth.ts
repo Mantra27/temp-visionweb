179a987b928a3e3e5770f21faf2912c6a2c1a5df
Mantra Gohil <surge@Mantras-MacBook-Air.local>
2022-05-29 02:48:18 +0530

    new schema

--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -10,43 +10,43 @@ router.post("/register", async (req:any, res:any)=>{
     const {username, email, password, country, contactNumber} = req.body;
     if(!username || !email || !password || !contactNumber || !country) return res.status(404).send({message: "any of the query is missing from the client's end."});
     
-    let isUsernameUNQ;
-
-    await User.findOne({username : username}).exec((err:any,user:any)=>{
-        console.log(user);   
-        if(!user) isUsernameUNQ = true;
-    });
     
-    let isEmailUNQ;
-    await User.findOne({email : email}).exec((err:any,user:any)=>{
-        console.log(user);   
-        if(!user) isEmailUNQ = true;
-    });
-
-    console.log(isUsernameUNQ, isEmailUNQ);
+    await User.findOne({email : email}).exec(async (err2:any,user:any)=>{
+        if(!user){
+        	await User.findOne({username : username}).exec(async (err2:any,user2:any)=>{
+        		if(!user2){
+        			const setNewUser = new User({
+                        username: username,
+                        email: email,
+                        password: password,
+                        country: country,
+                        contactNumber: contactNumber
+                    });
+                        bcrypt.genSalt(10, (err:any,salt:any)=> 
+                                bcrypt.hash(setNewUser.password,salt, (err:any, hash:any)=> {
+                                        if(err) return res.status(404).send({message:"error generating hash from the server, mail us on nandan@dicot.in"});
+                                        setNewUser.password = hash;
+                                        setNewUser.save()
+                                        .then((value:any)=>{
+                                            console.log('user succesfully registered');
+                                            return res.json({status: 'ok', message:'user registered'})
+                                        })
+                                        .catch((value:any) => console.log(value));
+                        }));
+        		}
+        		else{
+        			console.log("user already exists(same username)");
+                    return res.status(404).send({message: "user already exists(same username)"})
+        		}
+    		});
     
-    // const setNewUser = new User({
-    //     username: username,
-    //     email: email,
-    //     password: password,
-    //     country: country,
-    //     contactNumber: contactNumber
-    // });
-
-    //     bcrypt.genSalt(10, (err:any,salt:any)=> 
-    //             bcrypt.hash(setNewUser.password,salt, (err:any, hash:any)=> {
-    //                     if(err) return res.status(404).send({message:"error generating hash from the server, mail us on nandan@dicot.in"});
-    //                     setNewUser.password = hash;
-    //                     setNewUser.save()
-    //                     .then((value:any)=>{
-    //                         console.log('user succesfully registered');
-    //                         return res.json({status: 'ok', message:'user registered'})
-    //                     })
-    //                     .catch((value:any) => console.log(value));
-                        
-    //     }));
-
+        }
+        else{
+        	console.log("user already exists(same email)");
+            return res.status(404).send({message: "user already exists(same email)"})
+        }
     });
+});
 
 // router.post("/login", (req:any, res:any)=>{
 //     console.log("trying to log in");
